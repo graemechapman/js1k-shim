2e519899f22701913879adb561a57dad25b18475
Graeme Chapman <graemechapman41@gmail.com>
2018-03-16 23:21:12 +0000

    more snake

--- src/app.js
+++ src/app.js
@@ -19,52 +19,58 @@
   o = (d,p) => d == 0 ? 10 : d == 2 ? -10 : 0 // get segment direction offset
 
   r = (d,p) => { //run loop
-    s.splice(1, 0, z); // add a new element to the snake, in the right direction
-    s.pop(); // remove the last section of the snake
-
-    y -= o(z);
-    x += o(z-1);
-
-    a.fillStyle='#fff';
-    a.fillRect(0,0,500,500); // fill white
-    a.strokeRect(0,0,500,500); // add a border
-    a.fillStyle='#0f0';
-
-    a.fillText('🍏',e,f); //draw the apple
-    a.fillStyle='red';
+    console.log(x,e,y, f);
 
     u = x;
     v = y;
 
-    s.map((d,p) => { // draw out all the snakey bits
-        if (u == e && v == f) {
-            q=q*.95;
-            s.push(z);
-            e=m()
-            f=m();
-        }
+    //apple detection
+    if (x == e && y == f) {
+        q=q*.95;
+        s.push(z)
+        e=m()
+        f=m()
+    }
+    // move snake in the right direction
+    y -= o(z);
+    x += o(z-1);
 
+
+    a.fillStyle='#fff';
+    a.fillRect(0,0,500,500); // fill white
+    a.strokeRect(0,0,500,500); // add a border
+    a.fillStyle='#6f0';
+
+    a.fillText('🍏',e,f+8); //draw the apple - with your offset as it appears above the line
+    a.fillStyle='red';
+
+
+    a.fillRect(u, v, 10, 10);
+
+    s.map((d,p) => { // draw out all the snakey bits
         u -= o(d-1); // increment snake segment based on direction
         v += o(d);
 
-        (p > 1 && u == x && v == y)?h=1:0;
+        h=(u == x && v == y);
 
         a.fillRect(u, v, 10, 10);
     });
+    s.splice(0, 0, z); // add a new element to the snake, in the right direction
+    s.pop(); // remove the last section of the snake
 
-    [x,y].map((d) => d < 0 || d > 480?h=1:0) // wall collision detection
+    [x,y].map((d) => d < 0 || d > 490?h=1:0) // wall collision detection
 
     i++;
 
     a.fillText(i*100, 10,10); //write score
 
-    h ? 0 : w.setTimeout(r, 150*q);
+    h ? 0 : w.setTimeout(r, 250*q);
   }
 
   w.onkeydown = (d) => {
     // z = (direction = ['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'].indexOf(d.key)) >= 0 ? Math.abs(direction - z) != 2 ? direction : z : z;
-    z = (direction = [38, 39, 40, 37].indexOf(d.keyCode)) >= 0 ? Math.abs(direction - z) != 2 ? direction : z : z;
+    z = (direction = [38, 39, 40, 37].indexOf(d.keyCode)) >= 0 ? Math.abs(direction - s[0]) != 2 ? direction : z : z;
   };
 
-  w.setTimeout(r, 150*q);
+  w.setTimeout(r, 250*q);
 })(window);
