1df411dd9a7272256a76424291a70f0805c05f50
Graeme Chapman <graemechapman41@gmail.com>
2018-03-18 00:23:04 +0000

    More optimisations

--- src/app.js
+++ src/app.js
@@ -4,40 +4,41 @@
 
   i = 0; //score
 
-  m = (d) => Math.floor(Math.random() * 48) * 10 + 15;
+  m = (d) => ~~(Math.random() * 48) * 10 + 15;
 
-  x = 245 // snake x coords
-  y = 245 // y coords
-  z = 0 // direction
-
-  s = [0]; // snake parts - values assigned to directions [up, right, down, left]
+  // x = snake x coords
+  // y = snake y coords
+  // q = game speed
+  x = y = q = 245; // snake x coords
+  z = 0; // direction - start moving left
+  k = 0; // last key pressed
+  s = [0]; // snake parts - values assigned to directions [left, up, right, down]
   e = m(); // apple x
   f = m(); // apple y
-  q = 250; // game speed
-  h = 0; // game running
+  h = 0; // game running (1 = game over)
 
-  o = (d) => d == 0 ? 10 : d == 2 ? -10 : 0 // get segment direction offset
+  o = (d) => d == 0 ? -10 : d == 2 ? 10 : 0 // get segment direction offset
 
   r = (d) => { //run loop
+    k < 4 && Math.abs(z- k) != 2 && (z = k); // check key pressed
     u = x;
     v = y;
 
     //apple detection
     if (e == x && y == f) {
         q*=.95;
-        s.push(s[i])
-        e=m()
-        f=m()
+        s.push(s[i]);
+        e=m();
+        f=m();
     }
 
     // move snake in the right direction
-    y -= o(z);
-    x += o(z-1);
+    x += o(z);
+    y += o(z-1);
 
     a.fillStyle='#fff';
     a.fillRect(0,0,500,500); // fill white
     a.strokeRect(0,0,500,500); // add a border
-    // a.fillStyle='#ff0';
 
     a.fillStyle='#f00';
     a.fillText('🍏',e,f+8); //draw the apple - with y offset as it appears above the line
@@ -45,10 +46,10 @@
     a.fillRect(u, v, 10, 10); //draw snake head
 
     s.map((d) => { // draw out all the snakey bits
-        u -= o(d-1); // increment snake segment based on direction
-        v += o(d);
+        u -= o(d); // move each snake segment based on direction
+        v -= o(d-1);
 
-        h=(u == x && y == v);
+        h=(u == x && y == v); // collision detect with snake head
 
         a.fillRect(u, v, 10, 10);
     });
@@ -65,7 +66,7 @@
     h || window.setTimeout(r,q);
   }
 
-  b.onkeydown = (d) => z = (direction = [38, 39, 40, 37].indexOf(d.which)) >= 0 ? Math.abs(direction - s[0]) != 2 ? direction : z : z;
+  b.onkeydown = (d) => k = d.which % 37; // modulo of 37 gives us 0-4 in order left, up, right, down
 
   window.setTimeout(r,q);
 })();
