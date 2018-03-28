e99d55f01edbd72b3b0809620aa545086e1dd882
Graeme Chapman <graeme.chapman@infinity.co>
2018-03-28 22:59:53 +0100

    482 bytes

--- src/app.js
+++ src/app.js
@@ -21,16 +21,17 @@
     o = (d) => (d&1 && d-2)*10 // get segment direction offset
 
     r = (d) => { //run loop
-        k < 5 && s[0]&1^k&1 && (s[0] = k); // check key pressed
-
-        c.height = c.width = 500; // hax... resetting canvas width clears canvas and all properties (font, colours etc)
+        s.unshift(s[0]); //add a new snake section
 
         //apple detection
         if (e == x && y == f) {
-            s.push(s[i]);
-            e = ~~(Math.random() * 48) * 10 + 15;
+            e = ~~(Math.random() * 48) * 10 + 15; // move the apple
             f = ~~(Math.random() * 48) * 10 + 15;
-        }
+        } else s.pop(); // remove the end section unless it's eaten an apple
+
+        k < 5 && s[0]&1^k&1 && (s[0] = k); // check key pressed
+
+        c.height = c.width = 500; // hax... resetting canvas width clears canvas and all properties (font, colours etc)
 
         // move snake in the right direction
         u = x += o(s[0]);
@@ -38,9 +39,11 @@
 
         a.strokeRect(0,0,500,500); // add a border
 
-        a.fillStyle='#0f0';
-        a.fillRect(e,f,10,10);
         a.fillStyle='#f00';
+
+        a.fillText(i++*100, 10,15); //increment and write score
+        a.fillRect(e,f,10,10);
+        a.fillStyle='#0f0';
 
         s.map((d) => { // draw out all the snakey bits
             a.fillRect(u -= o(d), v -= o(d-1), 10, 10); // move each snake segment based on direction and draw them
@@ -48,13 +51,7 @@
             h = x < 0 || 490 < x || y < 0 || 490 < y || (u == x && y == v); // collision detection
         });
 
-        s.unshift(s[0]); // add a new element to the snake, in the right direction
-        s.pop(); // remove the last section of the snake
-
-        a.fillText(i++*100, 10,15); //increment and write score
-
         h || setTimeout(r,q);
-
     }
 
     b.onkeydown = (d) => (k = d.which % 36) && h && z(); // modulo of 36 gives us 1-4 in order left, up, right, down
