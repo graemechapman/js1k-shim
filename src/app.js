d607def73dd81f1c3d5075a9acdc93d104dbdbcd
Graeme Chapman <graeme.chapman@infinity.co>
2018-03-28 23:39:08 +0100

    Move high score to the right

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
  * Snake in under 650 bytes
  */
 ((d) => {
-    localStorage.s || (localStorage.s = 0);
     // start game
     z = (d) => {
         // x = snake x coords
@@ -23,53 +22,53 @@
     o = (d) => (d&1 && d-2)*10 // get segment direction offset
 
     r = (d) => { //run loop
-        k < 5 && s[0]&1^k&1 && (s[0] = k); // check key pressed
 
-        c.height = c.width = 500; // hax... resetting canvas width clears canvas and all properties (font, colours etc)
-        a.lineWidth = 10; 
-        a.lineJoin = a.lineCap = `round`;
+        s.unshift(s[0]); //add a new snake section
 
         //apple detection
         if (e == x && y == f) {
-            q*=.95;
-            s.push(s);
-            e = ~~(Math.random() * 48) * 10 + 15;
+            e = ~~(Math.random() * 48) * 10 + 15; // move the apple
             f = ~~(Math.random() * 48) * 10 + 15;
-        }
+        } else s.pop(); // remove the end section unless it's eaten an apple
+
+        k < 5 && s[0]&1^k&1 && (s[0] = k); // check key pressed
+
+        c.height = c.width = 500; // hax... resetting canvas width clears canvas and all context properties (font, colours etc)
+        a.lineWidth = 10;
+        a.lineJoin = a.lineCap = `round`;
 
         // move snake in the right direction
         u = x += o(s[0]);
-        v = y += o(s[0]-1);
+        v = y += o(s[0] - 1);
 
-        a.strokeRect(0,0,500,500); // add a border
+        a.strokeRect(0, 0, 500, 500); // add a border
 
-        a.fillText(`🍎`,e,f+8); //draw the apple - with y offset as it appears above the line
+        a.fillText(`🍎`, e, f+8); // draw the apple - with y offset as it appears above the line
 
         a.beginPath();
         a.moveTo(u+5,v+5);
 
         s.map((d) => { // draw out all the snakey bits
             u -= o(d); // move each snake segment based on direction
-            v -= o(d-1);
-            a.lineTo(u+5,v+5);
+            v -= o(d - 1);
+            a.lineTo(u + 5, v + 5);
 
             h = x < 0 || 490 < x || y < 0 || 490 < y || (u == x && y == v); // collision detection
         });
+
         a.strokeStyle=`#0f0`;
         a.stroke();
 
-        s.unshift(s[0]); // add a new element to the snake, in the right direction
-        s.pop(); // remove the last section of the snake
+        a.fillText(i+=100, 10, 15); //write score
 
-        a.fillText(++i*100, 10,15); //write score
+        a.fillText(`⭐ ${localStorage.s}`, 480 - 8 * `${localStorage.s}`.length, 15); //write high score
 
-        h && localStorage.s < i && (localStorage.s = i);
+        h && ~~localStorage.s < i && (localStorage.s = i);
 
         h || setTimeout(r,q);
 
-        localStorage.s && a.fillText(`⭐ ${localStorage.s*100}`, 10,35); //write high score
         a.font=`100px A`; // more hax.. no space to give a real font name, but it needs something
-        h && a.fillText(`☠️`,180,245);
+        h && a.fillText(`☠️`, 180, 270);
     }
 
     b.onkeydown = (d) => (k = d.which % 36) && h && z(); // modulo of 36 gives us 1-4 in order left, up, right, down
