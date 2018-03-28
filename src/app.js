6cb8958f67e642990709cb33a45f6fa58acccd50
Graeme Chapman <graeme.chapman@infinity.co>
2018-03-28 18:46:12 +0100

    Reduce file size and add more emoji

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // self executing anonymous, entry point for your app
 ((d) => {
-    c.width = c.height = 500; // set canvas size
+    c.height = c.width = 500; // set canvas size
 
     g = 0; //high score
 
@@ -21,13 +21,14 @@
         setTimeout(r,q);
     }
 
-    a.lineWidth=10;
-    a.lineJoin=a.lineCap=`round`;
-
     o = (d) => (d&1 && d-2)*10 // get segment direction offset
 
     r = (d) => { //run loop
         k < 5 && s[0]&1^k&1 && (s[0] = k); // check key pressed
+
+        c.width = 500; // hax... resetting canvas width clears canvas and all properties (font, colours etc)
+        a.lineWidth = 10; 
+        a.lineJoin = a.lineCap = `round`;
 
         //apple detection
         if (e == x && y == f) {
@@ -41,15 +42,13 @@
         u = x += o(s[0]);
         v = y += o(s[0]-1);
 
-        a.fillStyle=`#fff`;
-        a.fillRect(0,0,500,500); // fill white
         a.strokeRect(0,0,500,500); // add a border
 
-        a.fillStyle=`#f00`;
-        a.fillText(`🍏`,e,f+8); //draw the apple - with y offset as it appears above the line
+        a.fillText(`🍎`,e,f+8); //draw the apple - with y offset as it appears above the line
 
         a.beginPath();
         a.moveTo(u+5,v+5);
+        
         s.map((d) => { // draw out all the snakey bits
             u -= o(d); // move each snake segment based on direction
             v -= o(d-1);
@@ -57,24 +56,24 @@
 
             h = x < 0 || 490 < x || y < 0 || 490 < y || (u == x && y == v); // collision detection
         });
-        a.strokeStyle=`#f00`;
+        a.strokeStyle=`#0f0`;
         a.stroke();
-        a.strokeStyle=`#000`;
 
         s.unshift(s[0]); // add a new element to the snake, in the right direction
         s.pop(); // remove the last section of the snake
 
         a.fillText(++i*100, 10,15); //write score
 
-        h && a.fillText(`GAME OVER`,210,245);
         h && g < i && (g = i);
 
         h || setTimeout(r,q);
 
-        g && a.fillText(`hi ${g*100}`, 10,35); //write high score
+        g && a.fillText(`⭐ ${g*100}`, 10,35); //write high score
+        a.font=`100px A`; // more hax.. no space to give a real font name, but it needs something
+        h && a.fillText(`☠️`,180,245);
     }
 
-    b.onkeydown = (d) => (k = d.which % 36) && h && z(); // modulo of 36 gives us 1-5 in order left, up, right, down
+    b.onkeydown = (d) => (k = d.which % 36) && h && z(); // modulo of 36 gives us 1-4 in order left, up, right, down
 
     z();
 })();
